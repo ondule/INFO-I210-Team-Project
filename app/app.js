e45a748ac09cdb225753f2fc3f8abd145e151054
Jesús <andradejesus@outlook.com>
2023-04-24 13:20:22 -0400

    4/24 - Store items description and styling - JA
    Also began work on JS for store

--- app/app.js
+++ app/app.js
@@ -1,12 +1,59 @@
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+$(function () {
+    $("#bag-items").slideUp();
+    $(".bag").on("click", function () {
+      $("#bag-items").slideToggle();
+    });
+  
+    $("#items-basket").text("(" + $("#list-item").children().length + ")");
+  
+    $(".item-details").on("click", function () {
+      $("#cart-items").slideDown();
+      setTimeout(function () {
+        $("#bag-items").slideUp();
+      }, 2000);
+      //add items to basket
+      $(this).each(function () {
+        var name = $(this).children(".item-detail").children("h4").text();
+        var remove = "<button class='remove'> X </button>";
+        var cena =
+          "<span class='eachPrice'>" +
+          parseFloat(
+            $(this)
+              .children(".item-detail")
+              .children(".prices")
+              .children(".price")
+              .text()
+          ) +
+          "</span>";
+        $("#list-item").append(
+          "<li>" + name + "&#09; - &#09;" + cena + "$" + remove + "</li>"
+        );
+  
+        //number of items in basket
+        $("#items-basket").text("(" + $("#list-item").children().length + ")");
+        $("#items-basket").text();
+  
+        //calculate total price
+        var totalPrice = 0;
+        $(".eachPrice").each(function () {
+          var cenaEach = parseFloat($(this).text());
+          totalPrice += cenaEach;
+        });
+        $("#total-price").text(totalPrice + "$");
+      });
+  
+      //remove items from basket
+      $(".remove").on("click", function () {
+        $(this).parent().remove();
+  
+        var totalPrice = 0;
+        $(".eachPrice").each(function () {
+          var cenaEach = parseFloat($(this).text());
+          totalPrice += cenaEach;
+        });
+        $("#total-price").text(totalPrice + "$");
+        $("#items-basket").text("(" + $("#list-item").children().length + ")");
+      });
+    });
+  });
+  
