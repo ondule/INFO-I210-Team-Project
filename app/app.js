0b8c73e45fd7fa30920a390bf89ac5baae55be19
Jesús <andradejesus@outlook.com>
2023-04-24 15:09:10 -0400

    4/24 jQuery Add to Bag - JA
    Code working, items and prices are added to cart when clicked, basket still needs styling.

--- app/app.js
+++ app/app.js
@@ -1,59 +1,49 @@
-$(function () {
-    $("#bag-items").slideUp();
-    $(".bag").on("click", function () {
-      $("#bag-items").slideToggle();
-    });
+$(function(){
+
+  $("#cart-items").slideUp();
+  $(".cart").on("click", function () {
+  $("#cart-items").slideToggle();
+  });
+
+  $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+
   
-    $("#items-basket").text("(" + $("#list-item").children().length + ")");
-  
-    $(".item-details").on("click", function () {
-      $("#cart-items").slideDown();
-      setTimeout(function () {
-        $("#bag-items").slideUp();
-      }, 2000);
-      //add items to basket
-      $(this).each(function () {
-        var name = $(this).children(".item-detail").children("h4").text();
-        var remove = "<button class='remove'> X </button>";
-        var cena =
-          "<span class='eachPrice'>" +
-          parseFloat(
-            $(this)
-              .children(".item-detail")
-              .children(".prices")
-              .children(".price")
-              .text()
-          ) +
-          "</span>";
-        $("#list-item").append(
-          "<li>" + name + "&#09; - &#09;" + cena + "$" + remove + "</li>"
-        );
-  
-        //number of items in basket
-        $("#items-basket").text("(" + $("#list-item").children().length + ")");
-        $("#items-basket").text();
-  
+  $(".item").on("click", function () {
+    $("#cart-items").slideDown();
+   setTimeout(function(){
+      $("#cart-items").slideUp();
+   }, 3000)
+    //add items to basket
+    $(this).each(function () {
+      var name = $(this).children(".item-details").children("h4").text();
+      var remove = "<button class='remove'> X </button>";
+      var cena = "<span class='eachPrice'>" + (parseFloat($(this).children(".item-details").children(".prices").children(".price").text())) + "</span>";
+      $("#list-item").append("<li>" + name + "&#09; - &#09;" + cena + "$" + remove + "</li>");
+
+      //number of items in basket
+      $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+      $("#items-basket").text();
+      
         //calculate total price
         var totalPrice = 0;
-        $(".eachPrice").each(function () {
-          var cenaEach = parseFloat($(this).text());
-          totalPrice += cenaEach;
-        });
-        $("#total-price").text(totalPrice + "$");
-      });
-  
-      //remove items from basket
-      $(".remove").on("click", function () {
-        $(this).parent().remove();
-  
-        var totalPrice = 0;
-        $(".eachPrice").each(function () {
-          var cenaEach = parseFloat($(this).text());
-          totalPrice += cenaEach;
-        });
-        $("#total-price").text(totalPrice + "$");
-        $("#items-basket").text("(" + $("#list-item").children().length + ")");
-      });
+          $(".eachPrice").each(function (){ 
+            var cenaEach = parseFloat($(this).text());
+            totalPrice+=cenaEach;
+          });
+          $("#total-price").text(totalPrice + "$");
+    });
+
+    //remove items from basket
+    $(".remove").on("click", function () {
+      $(this).parent().remove();
+
+          var totalPrice = 0;
+          $(".eachPrice").each(function (){ 
+            var cenaEach = parseFloat($(this).text());
+            totalPrice+=cenaEach;
+          });
+          $("#total-price").text(totalPrice + "$");
+      $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
     });
   });
-  
+})
